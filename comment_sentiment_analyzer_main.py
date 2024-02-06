ad177690b3413a1c08edc3a2fd75c4f1bf7d527a
ikanar <ian.kanar@gmail.com>
2024-02-06 11:13:09 -0500

    Update
    Added user input of youtube link,
    now prints out comments with their compounded score as well as the average at the end.

--- comment_sentiment_analyzer_main.py
+++ comment_sentiment_analyzer_main.py
@@ -4,15 +4,38 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 
+def get_comment_sentiment(downloader,analyzer,link):
+
+    comments = downloader.get_comments_from_url(link)
+    
+    sentiment = []
+
+    average = 0.0
+    count = 0.0
+    for comment in comments:
+        score = analyzer.polarity_scores(comment['text'])
+        average = average + score['compound']
+        count = count + 1.0
+        sentiment.append([comment['text'],score['compound']])
+    average = average/count
+    sentiment.append(average)
+    return sentiment    
+
 
 def main():
     nltk.download('vader_lexicon')
     downloader = YoutubeCommentDownloader()
-    comments = downloader.get_comments_from_url('ENTER YOUTUBE LINK HERE')
+    link = input('Input YouTube Link here: ')
     analyzer = SentimentIntensityAnalyzer()
-    for comment in comments:
-        print(comment['text'])
-        print(analyzer.polarity_scores(comment['text']))
+    sentiment = get_comment_sentiment(downloader,analyzer,link)
+
+    for score in sentiment[:-1]:
+        print('Comment: ' + score[0])
+        print('Score: ' + str(score[1])+'\n\n\n')
+
+    print("Average Score: " + str(sentiment[-1]))    
+
+
 
     
 if __name__ == '__main__':
