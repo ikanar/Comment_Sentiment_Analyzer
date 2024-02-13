6827c4297083a946976d5af018f3e7dff20def7b
ikanar <ian.kanar@gmail.com>
2024-02-12 23:55:14 -0500

    Update Comment_Sentiment_Analyzer
    Added Comments

--- comment_sentiment_analyzer_main.py
+++ comment_sentiment_analyzer_main.py
@@ -2,6 +2,7 @@ from youtube_comment_downloader import *
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
+from lyricsgenius import Genius
 
 
 
@@ -14,6 +15,8 @@ def get_comment_sentiment(downloader,analyzer,link,range):
     average = 0.0
     count = 0.0
 
+    #sets up Dictionary to hold sentiments
+    #Sentiment field is 0 for neutral, 1 for positive, -1 for negative
     sentiments = {'text':[],
                   'pos':[],
                   'neg':[],
@@ -21,23 +24,28 @@ def get_comment_sentiment(downloader,analyzer,link,range):
                   'compound':[],
                   'sentiment':[]}
 
+    #iterates through the comments and assigns them a sentiment score,
     for comment in comments:
-        
+        #get the comment text
         text = comment['text']
+
+        #puts text through analyzer and receives a score
         score = analyzer.polarity_scores(text)
 
+        #loads sentiment scores into a dictionary
         sentiments['text'].append(text)
         sentiments['pos'].append(score['pos'])
         sentiments['neg'].append(score['neg'])
         sentiments['neu'].append(score['neu'])
         sentiments['compound'].append(score['compound'])
-
+        compound_score = score['compound']
 
         print(score)
-        compound_score = score['compound']
+
+        #adds compound_score to average
         average = average + compound_score
         count = count + 1.0
-        sentiment.append([text,compound_score])
+        #sentiment.append([text,compound_score])
 
         negative_range = range * -1
         
@@ -52,13 +60,26 @@ def get_comment_sentiment(downloader,analyzer,link,range):
 
 
 def main():
+
+    #sets up analyzer, Lexicon, and Youtbue comment downloader
     nltk.download('vader_lexicon')
     downloader = YoutubeCommentDownloader()
+<<<<<<< Updated upstream
     link = input('Input YouTube Link here: ')
     range = input('Enter Sentiment Threshold: ')
 
     analyzer = SentimentIntensityAnalyzer()
     sentiment = get_comment_sentiment(downloader,analyzer,link,range)
+=======
+    analyzer = SentimentIntensityAnalyzer()
+    
+    #Gets user Input
+    link = input('Input YouTube Link here: ')
+
+    
+    #Gets sentiment of YOutube Comments
+    sentiment = get_comment_sentiment(downloader,analyzer,link)
+>>>>>>> Stashed changes
 
     print(sentiment)
 
